3a50c506df747196606e921249e5729274a259c1
L Zsofia <lakatos.zsofia1993@gmail.com>
2020-05-21 15:20:24 +0200

    xxx

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const circusRoutes = require("./routes/circus.routes");
 const ticketRoutes = require("./routes/ticket.routes");
+const path = require("path");
+require("dotenv").config();
 
+const uri = process.env.ATLAS_URI || "mongodb://localhost:27017/circus";
 // Connection for Mongoose
 const connect = () => {
   return mongoose
-    .connect("mongodb://localhost:27017/circus", {
+    .connect("uri", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
@@ -19,8 +22,14 @@ connect();
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 app.use("/circus", circusRoutes);
 app.use("/", ticketRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(5000, () => {
   console.log("app is running on 5000");
