f678ee143eb3a87a5eb3fbe6a1a3e7e29e415c5b
L Zsofia <lakatos.zsofia1993@gmail.com>
2020-05-25 09:47:23 +0200

    ..

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -4,49 +4,44 @@ import { Link } from "react-router-dom";
 import Button from "../Buttons/button";
 
 function Header() {
-    const [activePage, setActivePage] = React.useState("About Us");
-    const [menuOpen, setMenuOpen] = React.useState(false);
-    const links = [
-        "aboutus",
-        "circus",
-        "gallery",
-        "ticket"
-    ];
+  const [activePage, setActivePage] = React.useState("About Us");
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const links = ["aboutus", "circus", "gallery", "ticket"];
 
-    return (
-        <header className="header">
-            <Link className="header-logo-container" to="/aboutus">
-                <div>Wildcode Circus</div>
+  return (
+    <header className="header">
+      <Link className="header-logo-container" to="/aboutus">
+        <div>Wildcode Circus</div>
+      </Link>
+      <div className={menuOpen ? "open header-link" : "header-link"}>
+        {links.map((link) => {
+          return (
+            <Link to={`/${link}`}>
+              <li
+                className={
+                  activePage === link
+                    ? "active header-link-item"
+                    : "header-link-item"
+                }
+                onClick={() => (setActivePage(link), setMenuOpen(!menuOpen))}
+              >
+                {" "}
+                {link}{" "}
+              </li>
             </Link>
-            <div className={menuOpen ? "open header-link" : "header-link"}>
-                {links.map((link) => {
-                    return (
-                        <Link to={`/${link}`}>
-                            <li
-                                className={
-                                    activePage === link
-                                        ? "active header-link-item"
-                                        : "header-link-item"
-                                }
-                                onClick={() => setActivePage(link)}
-                            >
-                                {" "}
-                                {link}{" "}
-                            </li>
-                        </Link>
-                    );
-                })}
-            </div>
-            <label
-                className="hamburger-menu"
-                onClick={() => {
-                    setMenuOpen(!menuOpen);
-                }}
-            >
-                {!menuOpen ? "☰" : "x"}
-            </label>
-        </header>
-    );
+          );
+        })}
+      </div>
+      <label
+        className="hamburger-menu"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        {!menuOpen ? "☰" : "x"}
+      </label>
+    </header>
+  );
 }
 
 export default Header;
